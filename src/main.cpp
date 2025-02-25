99097f10a31ecb834c82eda50024503edaace650
Nick Bianco <nbianco@stanford.edu>
2025-02-24 20:13:55 -0800

    Add basic realization test

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,49 @@
 #include <benchmark/benchmark.h>
 #include <OpenSim/OpenSim.h>
-#include <Simbody.h>
 
-static void BM_LoadModel(benchmark::State& state) {
-    for (auto _ : state) {
-        try {
-            OpenSim::Model model("RajagopalLaiUhlrich2023.osim"); // Replace with an actual model
-        } catch (const std::exception& e) {
-            state.SkipWithError(e.what());
-        }
+class LoadModel : public benchmark::Fixture {
+public:
+    OpenSim::Model model;
+    SimTK::State state;
+
+    void SetUp(const ::benchmark::State& st) override {
+        OpenSim::Logger::setLevelString("off");
+        model = OpenSim::Model("RajagopalLaiUhlrich2023.osim");
+        state = model.initSystem();
+    }
+};
+
+BENCHMARK_DEFINE_F(LoadModel, BM_RealizePosition)(benchmark::State& st) {
+    for (auto _ : st) {
+        state.updY() = SimTK::Test::randVector(state.getY().size());
+        model.realizePosition(state);
     }
 }
 
-BENCHMARK(BM_LoadModel)->Unit(benchmark::kMillisecond);
+BENCHMARK_DEFINE_F(LoadModel, BM_RealizeVelocity)(benchmark::State& st) {
+    for (auto _ : st) {
+        state.updY() = SimTK::Test::randVector(state.getY().size());
+        model.realizeVelocity(state);
+    }
+}
+
+BENCHMARK_DEFINE_F(LoadModel, BM_RealizeDynamics)(benchmark::State& st) {
+    for (auto _ : st) {
+        state.updY() = SimTK::Test::randVector(state.getY().size());
+        model.realizeDynamics(state);
+    }
+}
+
+BENCHMARK_DEFINE_F(LoadModel, BM_RealizeAcceleration)(benchmark::State& st) {
+    for (auto _ : st) {
+        state.updY() = SimTK::Test::randVector(state.getY().size());
+        model.realizeAcceleration(state);
+    }
+}
+
+BENCHMARK_REGISTER_F(LoadModel, BM_RealizePosition)->Unit(benchmark::kMillisecond);
+BENCHMARK_REGISTER_F(LoadModel, BM_RealizeVelocity)->Unit(benchmark::kMillisecond);
+BENCHMARK_REGISTER_F(LoadModel, BM_RealizeDynamics)->Unit(benchmark::kMillisecond);
+BENCHMARK_REGISTER_F(LoadModel, BM_RealizeAcceleration)->Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN();
