6421a9c837b23a7d1bc914d78fb034c7af3ce840
nickbianco <nbianco@stanford.edu>
2025-04-28 12:42:28 -0500

    Testing out the CPodes integrator

--- src/sandbox/sandbox.cpp
+++ src/sandbox/sandbox.cpp
@@ -6,6 +6,107 @@
 
 using namespace SimTK;
 
+
+class AccelerationReporter : public ModelComponent {
+OpenSim_DECLARE_CONCRETE_OBJECT(AccelerationReporter, ModelComponent);
+
+public:
+    AccelerationReporter() : ModelComponent() {}
+
+protected:
+    void extendAddToSystem(SimTK::MultibodySystem& system) const override {
+        system.addEventReporter(new TextDataEventReporter
+            (system, new MyEvaluateAcceleration(), 0.001));
+    }
+
+private:
+    class MyEvaluateAcceleration : public TextDataEventReporter::UserFunction<Real> {
+    public:
+        Real evaluate(const System& system, const State& state) override {
+            const MultibodySystem& mbs = MultibodySystem::downcast(system);
+            mbs.realize(state, Stage::Acceleration);
+            return state.getUDot().normRMS();
+        }
+    };
+};
+class SmoothSphereHalfSpaceReporter : public ModelComponent {
+OpenSim_DECLARE_CONCRETE_OBJECT(SmoothSphereHalfSpaceReporter, ModelComponent);
+
+public:
+SmoothSphereHalfSpaceReporter() : ModelComponent() {}
+
+protected:
+    void extendConnectToModel(Model& model) override {
+        for (const auto& force : model.getComponentList<OpenSim::SmoothSphereHalfSpaceForce>()) {
+            m_forces.emplace_back(&force);
+        }
+    }
+
+    void extendAddToSystem(SimTK::MultibodySystem& system) const override {
+        system.addEventReporter(new TextDataEventReporter
+            (system, new MyEvaluateForces(m_forces), 0.001));
+    }
+
+private:
+    class MyEvaluateForces : public TextDataEventReporter::UserFunction<Vector> {
+    public:
+        MyEvaluateForces(const std::vector<SimTK::ReferencePtr<const OpenSim::SmoothSphereHalfSpaceForce>>& forces)
+            : m_forces(forces) {}
+
+        Vector evaluate(const System& system, const State& state) override {
+            const MultibodySystem& mbs = MultibodySystem::downcast(system);
+            mbs.realize(state, Stage::Acceleration);
+            SimTK::Vector forces(m_forces.size(), 0.0);
+            for (size_t i = 0; i < m_forces.size(); ++i) {
+                const auto& force = m_forces[i];
+                const auto& contactForce = force->getSphereForce(state);
+                forces[i] = contactForce.norm();
+            }
+
+            return forces;
+        }
+    private:
+        const std::vector<SimTK::ReferencePtr<const OpenSim::SmoothSphereHalfSpaceForce>>& m_forces;
+
+    };
+
+    std::vector<SimTK::ReferencePtr<const OpenSim::SmoothSphereHalfSpaceForce>> m_forces;
+};
+
+class VisualizerReporter : public ModelComponent {
+OpenSim_DECLARE_CONCRETE_OBJECT(VisualizerReporter, ModelComponent);
+
+public:
+    VisualizerReporter() : ModelComponent() {}
+
+protected:
+    void extendAddToSystem(SimTK::MultibodySystem& system) const override {
+        // system.setUseUniformBackground(true);
+        Visualizer viz(system);
+        system.addEventReporter(new Visualizer::Reporter(viz, 0.01));
+    }
+};
+
+
+void dumpIntegratorStats(double startCPU, double startTime, 
+        const Integrator& integ) {
+    std::cout << "DONE: Simulated " << integ.getTime() << " seconds in " <<
+    realTime()-startTime << " elapsed s, CPU="<< cpuTime()-startCPU << "s\n";
+
+    const int evals = integ.getNumRealizations();
+    std::cout << "\nUsed "  << integ.getNumStepsTaken() << " steps, avg step=" 
+    << (1000*integ.getTime())/integ.getNumStepsTaken() << "ms " 
+    << (1000*integ.getTime())/evals << "ms/eval\n";
+
+    printf("Used Integrator %s at accuracy %g:\n", 
+    integ.getMethodName(), integ.getAccuracyInUse());
+    printf("# STEPS/ATTEMPTS = %d/%d\n",  integ.getNumStepsTaken(), 
+                        integ.getNumStepsAttempted());
+    printf("# ERR TEST FAILS = %d\n",     integ.getNumErrorTestFailures());
+    printf("# REALIZE/PROJECT = %d/%d\n", integ.getNumRealizations(), 
+                        integ.getNumProjections());
+}
+
 int main() {
 
     using std::chrono::high_resolution_clock;
@@ -13,54 +114,42 @@ int main() {
     using std::chrono::duration;
     using std::chrono::microseconds;
 
-    Logger::setLevel(Logger::Level::Error);
 
     // Disable logging.
-    Model model("RajagopalFunctionBasedPathsDGFContact_nomuscles.osim");
+    Logger::setLevel(Logger::Level::Error);
+
+    // Load the model.
+    Model model("RajagopalFunctionBasedPathsDGFContact_noactdyn_nopassive.osim");
+    // Model model("RajagopalFunctionBasedPathsDGFContact_nomuscles.osim"); 
+    model.initSystem();
+    // model.addModelComponent(new AccelerationReporter());
+    // model.addModelComponent(new VisualizerReporter()); 
+    // model.addModelComponent(new SmoothSphereHalfSpaceReporter());
     SimTK::State state = model.initSystem();
+
+    const MultibodySystem& system = model.getMultibodySystem();
 
     // Helper function to reset the state.
     Vector defaultY = state.getY();
     auto resetState = [&](SimTK::State& state) {
         state.setTime(0);
         state.updY() = defaultY;
+        state.updU() = 0.1*SimTK::Test::randVector(state.getNU());
     };
-
-    const SimTK::MultibodySystem& system = model.getMultibodySystem();
-    system.realize(state, SimTK::Stage::Dynamics);
-    std::cout << "initial energy: " << system.calcEnergy(state) << std::endl;
     
-    SimTK::SemiExplicitEuler2Integrator integrator(system);
-    integrator.setFixedStepSize(0.001);
-    SimTK::TimeStepper timeStepper(system, integrator);
-    timeStepper.initialize(state);
-    auto start = high_resolution_clock::now();
-    timeStepper.stepTo(1.0);
-    auto end = high_resolution_clock::now();
-    double time_elapsed = duration_cast<microseconds>(end - start).count();
-    time_elapsed /= 1.0e6;
-
-    state = timeStepper.getState();
-    system.realize(state, SimTK::Stage::Dynamics);
-    std::cout << "final energy: " << system.calcEnergy(state) << std::endl;
-    std::cout << "TimeStepper time elapsed: " << time_elapsed << " seconds" << std::endl;
-
+    SimTK::CPodesIntegrator integ(system,
+        SimTK::CPodes::BDF, SimTK::CPodes::Newton);
+    // integ.setFixedStepSize(0.001);
+    // integ.setMinimumStepSize(1e-6);
+    SimTK::TimeStepper timeStepper(system, integ);
 
     resetState(state);
-    Manager manager(model);
-    // manager.setIntegratorMinimumStepSize(0.001);
-    // manager.setIntegratorMaximumStepSize(0.001);
-    manager.setIntegratorMethod(Manager::IntegratorMethod::RungeKuttaMerson);
-    manager.initialize(state);
-    start = high_resolution_clock::now();
-    manager.integrate(2.0);
-    end = high_resolution_clock::now();
-    time_elapsed = duration_cast<microseconds>(end - start).count();
-    time_elapsed /= 1.0e6;
-    std::cout << "Manager time elapsed: " << time_elapsed << " seconds" << std::endl;
+    timeStepper.initialize(state);
 
-    TimeSeriesTable table = manager.getStatesTable();
-    VisualizerUtilities::showMotion(model, table);
+    const double startCPU = cpuTime(), startTime = realTime();
+    timeStepper.stepTo(5.0);
+    dumpIntegratorStats(startCPU, startTime, integ);
+
 
 
 
