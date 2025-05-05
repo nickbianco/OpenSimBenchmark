7155ab8460697eb1079dd6fa2b5e2f5317ab2584
nickbianco <nbianco@stanford.edu>
2025-05-05 16:36:28 -0500

    Sandbox testing for different contact force parameters

--- src/sandbox/sandbox.cpp
+++ src/sandbox/sandbox.cpp
@@ -107,6 +107,98 @@ void dumpIntegratorStats(double startCPU, double startTime,
                         integ.getNumProjections());
 }
 
+Model loadModel(double stiffnessScale = 1.0,
+                double dissipationScale = 1.0,
+                double frictionScale = 1.0,
+                double smoothingScale = 1.0) {
+
+    Model model("RajagopalFunctionBasedPathsDGFContact_noactdyn_nopassive.osim");
+    // Model model("RajagopalFunctionBasedPathsDGFContact_nomuscles.osim"); 
+    model.initSystem();
+
+    for (auto& force : model.updComponentList<OpenSim::SmoothSphereHalfSpaceForce>()) {
+        force.set_stiffness(force.get_stiffness() * stiffnessScale);
+        force.set_dissipation(force.get_dissipation() * dissipationScale);
+        force.set_dynamic_friction(frictionScale * force.get_dynamic_friction());
+        force.set_static_friction(frictionScale * force.get_static_friction());
+        force.set_viscous_friction(frictionScale * force.get_viscous_friction());
+        force.set_hertz_smoothing(smoothingScale * force.get_hertz_smoothing());
+        force.set_hunt_crossley_smoothing(smoothingScale * force.get_hunt_crossley_smoothing());
+    }
+
+    return model;
+}
+
+void simulateSweep(std::string sweepParameter) {
+    std::vector<double> scaleSweep = {1e-2, 1e-1, 1.0, 10.0, 100.0};
+
+    std::vector<double> realTimes;
+    realTimes.reserve(scaleSweep.size());
+    std::vector<double> cpuTimes;
+    cpuTimes.reserve(scaleSweep.size());
+    std::vector<double> numSteps;
+    numSteps.reserve(scaleSweep.size());
+    std::vector<double> avgStepSizes;
+    avgStepSizes.reserve(scaleSweep.size());
+    for (const auto& scale : scaleSweep) {
+        std::cout << "Running simulation with scale: " << scale << std::endl;
+
+        // Load the model with the specified scale.
+        Model model = loadModel(
+            (sweepParameter == "stiffness") ? scale : 1.0,
+            (sweepParameter == "dissipation") ? scale : 1.0,
+            (sweepParameter == "friction") ? scale : 1.0,
+            (sweepParameter == "smoothing") ? scale : 1.0
+        );
+
+        // Initialize the system.
+        SimTK::State state = model.initSystem();
+
+        const MultibodySystem& system = model.getMultibodySystem();
+
+        // Helper function to reset the state.
+        Vector defaultY = state.getY();
+        auto resetState = [&](SimTK::State& state) {
+            state.setTime(0);
+            state.updY() = defaultY;
+            // state.updU() = 0.1*SimTK::Test::randVector(state.getNU());
+        };
+
+        // Create the integrator and time stepper.
+        SimTK::CPodesIntegrator integ(system,
+            SimTK::CPodes::BDF, SimTK::CPodes::Newton);
+        // SimTK::RungeKuttaMersonIntegrator integ(system);
+        integ.setAccuracy(1e-2);
+        SimTK::TimeStepper timeStepper(system, integ);
+
+        resetState(state);
+        timeStepper.initialize(state);
+
+        const double startCPU = cpuTime(), startTime = realTime();
+        
+        // Run the simulation for 5 seconds.
+        timeStepper.stepTo(5.0);
+        
+        // Save results.
+        realTimes.push_back(realTime() - startTime);
+        cpuTimes.push_back(cpuTime() - startCPU);
+        numSteps.push_back(integ.getNumStepsTaken());
+        avgStepSizes.push_back((1000 * integ.getTime()) / integ.getNumStepsTaken());
+    }
+
+    // Print the results.
+    std::cout << "\nResults for " << sweepParameter << " sweep:\n";
+    std::cout << "Scale\tReal Time (s)\tCPU Time (s)\tNum Steps\tAvg Step Size (ms)\n";
+    for (size_t i = 0; i < scaleSweep.size(); ++i) {
+        std::cout << scaleSweep[i] << "\t"
+                  << realTimes[i] << "\t"
+                  << cpuTimes[i] << "\t"
+                  << numSteps[i] << "\t"
+                  << avgStepSizes[i] << "\n";
+    }
+
+}
+
 int main() {
 
     using std::chrono::high_resolution_clock;
@@ -114,42 +206,13 @@ int main() {
     using std::chrono::duration;
     using std::chrono::microseconds;
 
-
     // Disable logging.
     Logger::setLevel(Logger::Level::Error);
 
-    // Load the model.
-    // Model model("RajagopalFunctionBasedPathsDGFContact_noactdyn_nopassive.osim");
-    Model model("RajagopalFunctionBasedPathsDGFContact_nomuscles.osim"); 
-    model.initSystem();
-    // model.addModelComponent(new AccelerationReporter());
-    // model.addModelComponent(new VisualizerReporter()); 
-    // model.addModelComponent(new SmoothSphereHalfSpaceReporter());
-    SimTK::State state = model.initSystem();
-
-    const MultibodySystem& system = model.getMultibodySystem();
-
-    // Helper function to reset the state.
-    Vector defaultY = state.getY();
-    auto resetState = [&](SimTK::State& state) {
-        state.setTime(0);
-        state.updY() = defaultY;
-        // state.updU() = 0.1*SimTK::Test::randVector(state.getNU());
-    };
-    
-    // SimTK::RungeKuttaMersonIntegrator integ(system);
-    SimTK::CPodesIntegrator integ(system,
-        SimTK::CPodes::BDF, SimTK::CPodes::Newton);
-    // integ.setFixedStepSize(0.001);
-    // integ.setMinimumStepSize(1e-6);
-    SimTK::TimeStepper timeStepper(system, integ);
-
-    resetState(state);
-    timeStepper.initialize(state);
-
-    const double startCPU = cpuTime(), startTime = realTime();
-    timeStepper.stepTo(1.0);
-    dumpIntegratorStats(startCPU, startTime, integ);
+    simulateSweep("stiffness");
+    // simulateSweep("dissipation");
+    // simulateSweep("friction");
+    // simulateSweep("smoothing");
 
 
 
